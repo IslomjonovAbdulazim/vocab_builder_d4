829d086f19b9523412fbc4966935ff2353fe1f57
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-25 19:03:52 +0500

    imp

--- lib/pages/folders/folders_widgets.dart
+++ lib/pages/folders/folders_widgets.dart
@@ -6,19 +6,21 @@ class _Folders extends GetView<FoldersController> {
   @override
   Widget build(BuildContext context) {
     return Obx(() {
-      return ListView.builder(
-        itemCount: controller.folders.length,
-        itemBuilder: (context, index) {
-          final model = controller.folders[index];
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4),
-            child: ListTile(
-              title: Text(model.name),
-              subtitle: Text(model.createdAt.toString()),
-            ),
-          );
-        },
-      );
+      return controller.isLoading.value
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: controller.folders.length,
+              itemBuilder: (context, index) {
+                final model = controller.folders[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: ListTile(
+                    title: Text(model.name),
+                    subtitle: Text(model.createdAt.toString()),
+                  ),
+                );
+              },
+            );
     });
   }
 }
