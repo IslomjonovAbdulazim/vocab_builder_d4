c66c4a96bb0a446cc9736780142589b06e932416
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-25 17:42:26 +0500

    imp

--- lib/models/folder_model.dart
+++ lib/models/folder_model.dart
@@ -1,1 +1,33 @@
+class FolderModel {
+  // members
+  late String id;
+  late String name;
+  late DateTime createdAt;
+
+  // constructor
+  FolderModel({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  // fromJson
+  FolderModel.fromJson(Map json) {
+    id = json["id"];
+    name = json["name"];
+    createdAt = DateTime.parse(json["createdAt"]);
+  }
+
+  // toJson
+  Map toJson() => {
+    "id": id,
+    "name": name,
+    "createdAt": createdAt.toIso8601String(),
+  };
+}
+
+
+
+
+
 
---
+++ lib/models/vocab_model.dart
@@ -0,0 +1,16 @@
+class VocabModel {
+  // members
+  late String id;
+  late String word;
+  late String meaning;
+
+  // constructor
+  VocabModel({
+    required this.id,
+    required this.word,
+    required this.meaning,
+  });
+
+  // fromJson
+
+}
